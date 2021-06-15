0ebbbe5b6c58e750dd482779e8a33efce3c54738
Jonas Frey Rosborg Schaarup <jonasschaarup@gmail.com>
2021-06-15 17:52:57 +0200

    Start of data wrangling

---
+++ R/wrangling-session.R
@@ -0,0 +1,11 @@
+
+# 9.3 Load the packages and the dataset
+library(tidyverse)
+library(NHANES)
+
+## # Load up the packages
+## source(here::here("R/package-loading.R"))
+
+# Briefly glimpse contents of dataset
+glimpse(NHANES)
+
