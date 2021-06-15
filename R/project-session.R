39d735e0b7b6ede7ef7103727db0642a576a60fd
Jonas Frey Rosborg Schaarup <jonasschaarup@gmail.com>
2021-06-15 14:55:04 +0200

    Initial commit

---
+++ R/project-session.R
@@ -0,0 +1,48 @@
+10
+source(here::here("R/package-loading.R"))
+
+# Basic of R --------------------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos
+
+c("a", "b", "c")
+c(TRUE, FALSE)
+c(1, 2, 4)
+factor(c("high", "medium", "low"))
+
+#Overview
+head(CO2, 10)
+
+#Variable names
+colnames(CO2)
+
+#Datastructure
+str(CO2)
+
+# Despritive overview
+summary(CO2)
+
+colnames(CO2)
+
+
+# Object names
+day_one
+false <- FALSE
+numeric_val <- 9
+
+# Spacing
+row_val[, 1]
+x[, 1]
+mean(x, na.rm = TRUE)
+mean(x, na.rm = TRUE)
+height <- (feet * 12) + inches
+df$z
+
+x <- 1:10
+
+# Indenting and brackets
+if (y < 0 && debug) {
+    message("Y is negative")
+}
+
