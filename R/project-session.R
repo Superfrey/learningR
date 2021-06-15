42b16519fa1d9a8f3f3aa96e36b018f1fee77e94
Jonas Frey Rosborg Schaarup <jonasschaarup@gmail.com>
2021-06-15 16:26:50 +0200

    add cool stuff

--- R/project-session.R
+++ R/project-session.R
@@ -11,13 +11,13 @@ c(TRUE, FALSE)
 c(1, 2, 4)
 factor(c("high", "medium", "low"))
 
-#Overview
+# Overview
 head(CO2, 10)
 
-#Variable names
+# Variable names
 colnames(CO2)
 
-#Datastructure
+# Datastructure
 str(CO2)
 
 # Despritive overview
@@ -26,23 +26,4 @@ summary(CO2)
 colnames(CO2)
 
 
-# Object names
-day_one
-false <- FALSE
-numeric_val <- 9
-
-# Spacing
-row_val[, 1]
-x[, 1]
-mean(x, na.rm = TRUE)
-mean(x, na.rm = TRUE)
-height <- (feet * 12) + inches
-df$z
-
-x <- 1:10
-
-# Indenting and brackets
-if (y < 0 && debug) {
-    message("Y is negative")
-}
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 TODO: Add more to the title of your project here
 
-# LearningR:
+# LearningR: Practicing cool stuff 
 
 TODO: Give a brief description of what your project is about
 
