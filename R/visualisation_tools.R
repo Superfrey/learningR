bc3421d5e0cd758f27a8f2960dbe3828c78bc04c
Jonas Schaarup <60611191+Superfrey@users.noreply.github.com>
2023-09-26 15:08:39 +0200

    Update visualisation_tools.R
    Ggplot visualisation tool

--- R/visualisation_tools.R
+++ R/visualisation_tools.R
@@ -1,1 +1,12 @@
-
+data %>% 
+  ggplot(aes(x = x_var, y = y_var)) + 
+  geom_jitter(aes(colour = factor(group_var))) +
+  #geom_line(aes(group = group_var, colour = factor(group_var))) +
+  geom_errorbar(aes(ymin = mean.akk - se.k, ymax = mean.akk + se.k, colour = factor(treatment), width = 1.9)) + 
+  
+  theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
+  xlab("x_label") +
+  ylab(expression("y_label")) +
+  labs(colour = "group_var")+
+  scale_color_manual(values = c("darkgoldenrod1", "darkorange", "chartreuse2","chartreuse4"))
